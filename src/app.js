ad21c25e93c1ba0280d32e5a85b9c62aa304e445
Javin Towers <towersjavin@gmail.com>
2024-01-23 16:23:55 -0500

    Update app.js - changed map center

--- src/app.js
+++ src/app.js
@@ -112,7 +112,7 @@ function initMap() {
   // Create the map.
   const map = new google.maps.Map(document.getElementById('map'), {
     zoom: 7,
-    center: {lat: 52.632469, lng: -1.689423},
+    center: {lat: 44.5588, lng: 72.5778},
     styles: mapStyle,
   });
 
